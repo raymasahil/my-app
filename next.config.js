657ab0d65d4633635acba8ff8fe18b459bfac849
dipeshkukadiya22 <dipesh@teque7.com>
2024-01-04 17:58:48 +0530

    next config changes

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+// next.config.js
+module.exports = {
+    output: {
+      // Other options...
+      export: true,
+    },
+    // Other configurations...
+  }
