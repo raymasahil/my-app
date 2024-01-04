0302c053bf28f1af313ca26d6cfee6d2a7e217ef
dipeshkukadiya22 <dipesh@teque7.com>
2024-01-04 13:11:43 +0530

    some changes

---
+++ src/app/(Components)/Header.tsx
@@ -0,0 +1,98 @@
+'use client';
+import { useState } from "react";
+import Link from "next/link";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <header className="!z-[99]">
+      <nav className="p-0">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            <div className="text-pink-500 !z-[999]">
+              <Link href="/"> Your Logo </Link>
+            </div>
+            <div className="flex text-pink-500 !z-[999]">
+              <div className="sm:hidden text-pink-500 cursor-pointer" onClick={toggleMenu}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M3.75 9h16.5m-16.5 6.75h16.5"}
+                  />
+                </svg>
+              </div>
+              <ul
+                className={`sm:flex p-4 space-x-7 items-center ${
+                  isMenuOpen ? "block fixed top-0 pt-4 z-10 right-0  w-full h-full bg-black" : "hidden"
+                }`}
+              >
+                <li className="sm:hidden absolute top-4 right-4">
+                  <div className="text-pink-500 cursor-pointer" onClick={closeMenu}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </div>
+                </li>
+                <li>
+                  <Link href="/" onClick={closeMenu}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/About" onClick={closeMenu}>
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/Contact" onClick={closeMenu}>
+                    Services
+                  </Link>
+                </li>
+                <li>
+
+                  <button className="mt-0 pt-2 pb-2 pl-4 pr-4 rounded-3xl text-white bg-pink-500 flex items-center">
+                    Contact Us
+                  </button>
+
+                </li>
+                
+              </ul>
+
+              
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/(Components)/MySwiper.tsx
@@ -0,0 +1,69 @@
+'use client';
+import React, { useRef, useState } from 'react';
+import Image from 'next/image';
+import { Button } from '@nextui-org/react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import '@/style/swiper.css';
+
+const MySwiper: React.FC = () => {
+    return(
+
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper !h-screen mt-[-72px] !z-[1]"
+        >
+
+          <SwiperSlide className="flex w-full justify-center items-center bg-[url('/Images/bg2-big.jpg')] bg-cover bg-no-repeat"> 
+            <div className='container mx-auto w-full h-full px-4 flex flex-row items-center'>
+                <div className='w-1/2 animate-fade-in'>
+                  <h1 className='w-72 text-4xl font-bold pb-4'>Your well-being is our priority</h1> 
+                  <p className='font-medium'>Promoting good health together.</p> 
+                  <button className="border border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white ransition-all duration-300 mt-3 pt-2 pb-2 rounded-3xl pl-4 pr-4 shadow-lg flex items-center">
+                    Button <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                      </svg>
+
+                  </button>
+                </div>
+                <div className='w-1/2'></div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide className="flex w-full justify-center items-center bg-[url('/Images/bg3-big.jpg')] bg-cover bg-no-repeat"> 
+            <div className='container mx-auto w-full h-full px-4 flex flex-row items-center'>
+                <div className='w-1/2 animate-fade-in'>
+                  <h1 className='w-[600px] text-4xl font-bold pb-4'>We're committed to your health and well-being</h1> 
+                  <p className='font-medium w-96'>We are dedicated to providing comprehensive healthcare services tailored specifically to address the unique needs of women.</p> 
+                  <button className="border border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white ransition-all duration-300 mt-3 pt-2 pb-2 rounded-3xl pl-4 pr-4 shadow-lg flex items-center">
+                    Button <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                      </svg>
+
+                  </button>
+                </div>
+                <div className='w-1/2'></div>
+            </div>
+          </SwiperSlide>
+              
+        
+      </Swiper>
+
+    )
+}
+
+export default MySwiper;
