dd8886e84bc2931206e31c78e1f41e915143dac6
dipeshkukadiya22 <dipesh@teque7.com>
2024-01-08 17:38:02 +0530

    updated next.config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Your other Next.js configuration options...
+  
+    // Add the following exportPathMap configuration
+    exportPathMap: async function () {
+      return {
+        '/': { page: '/' },
+        // Add other pages as needed
+      }
+    },
+  }
+  
+  module.exports = nextConfig;
+  
