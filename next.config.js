ffa89b1225e6de20af5e9610d82d53520fc9296a
dipeshkukadiya22 <dipesh@teque7.com>
2024-01-09 10:42:48 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
-// next.config.js
-
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    // Your other Next.js configuration options...
-  
-    // Replace exportPathMap with generateStaticParams
-    async generateStaticParams() {
-      // Specify the paths you want to export
-      return [
-        '/',
-        // Add other paths as needed
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+ 
+ 
+}
+ 
+module.exports = nextConfig
