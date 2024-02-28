538dcc60c113c2d870644182dab8ed89a5a88958
rupalteque7 <rupal@teque7.com>
2024-02-28 23:25:13 +0530

    about and meetteam changes

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,7 +1,62 @@
+import Image from 'next/image'
+import Link from 'next/link'
 export default function About() {
-    return(
-        <div>
-            <p>About Page</p>
+    return (
+     
+        <div className="main-content" >
+            <div className='md mx-auto relative'>
+                <Image
+                    src="/images/downloadbanner.jpg"
+                    width={500}
+                    height={200}
+                    alt="Picture of the author"
+                    className="w-full h-auto"
+                />
+                <h1 className="absolute bottom-20 left-0 text-white px-8 py-2 text-6xl">About Us</h1>
+                <p className="absolute bottom-10 left-0 text-white px-8 py-2">
+                    <Link href="/">Home</Link> &gt; <Link href="/About">About us</Link>
+                </p>
+            </div>
+            <div className='container max-w-[1280px] mx-auto mt-14 mb-24'>
+                <div className="box-border h-auto w-full p-4 flex">
+                    <div>
+                        <img width={540} height={300} src="images/Surabhi-Bg.png" alt="Dr. Surabhi Vegad"/>
+                    </div>
+                    <div className="ml-24 mt-4">
+                        <h1 className="text-4xl text-left ml-4 font-bold text-green-500" style={{ color: 'rgb(1, 146, 144)'}}>Dr. Surabhi Vegad</h1>
+                            <div className="box-border h-auto p-4">
+                                <div className="flex items-start mt-5 leading-loose text-lg">
+                                    <p className="box-border w-20 font-bold  mr-4" style={{ color: 'rgb(1, 146, 144)'}}>Speciality</p>
+                                    <ul className="flex-grow  ml-3">
+                                        <li>High Risk Pregnancy Care</li>
+                                        <li>Advanced Gynological Laparoscopy</li>
+                                        <li>Hysteroscopy Procedures</li>
+                                        <li>In-vitro fertilization(IVF)</li>
+                                        <li>Intrauterine Insemination(IUI)</li>
+                                        <li>Comprehensive Infertility Solutions</li>
+                                    </ul>
+                                </div>
+                                <div className="flex items-start mt-2 leading-loose text-lg">
+                                    <p className=" box-border w-20 font-bold mr-4" style={{ color: 'rgb(1, 146, 144)'}}>Education</p>
+                                    <ul className="flex-grow  ml-3">
+                                        <li>Dip in Cosmetic Gynecology Training, Florida, USA October 2004</li>
+                                        <li>Dip in Laparoscopic Gynecology, Kiel, Germany October 2004</li>
+                                        <li>Dip in Laparoscopic Gynecology, Kiel, Germany October 2004</li>
+                                    </ul>
+                                </div>
+                                <div className="flex items-start mt-2 leading-loose text-lg">
+                                    <p className=" box-border w-20 font-bold mr-4" style={{ color: 'rgb(1, 146, 144)'}}>Experience</p>
+                                    <p className="w-20flex-grow  ml-3">20 years</p>
+                                </div>
+                                <div className="flex items-start mt-2 leading-loose text-lg">
+                                    <p className="box-border w-20   font-bold mr-4" style={{ color: 'rgb(1, 146, 144)'}}>Available</p>
+                                    <p className="flex-grow  ml-3" >Monday-Saturday</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
         </div>
-    )
+     
+    );
 }
---
+++ src/app/MeetTeam/page.tsx
@@ -0,0 +1,81 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone} from '@fortawesome/free-solid-svg-icons'
+import { faPaintBrush } from '../../../node_modules/@fortawesome/free-solid-svg-icons/index'
+export default function Contact() {
+    return(
+        <div className="main-content">
+           <div className='md mx-auto relative'>
+                <Image
+                    src="/images/downloadbanner.jpg"
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"
+                    className="w-full h-auto"
+                />
+                <h1 className="absolute bottom-20 left-0 text-white px-8 py-2 text-6xl">Meet Team</h1>
+                <p className="absolute bottom-10 left-0 text-white px-8 py-2">
+                    <Link href="/">Home</Link> &gt; <Link href="/MeetTeam">Meet Team</Link>
+                </p>
+            </div>
+            <div className='container max-w-[1280px] mx-auto mt-14 mb-24'>
+            <div className="flex justify-center">
+                <div className="grid grid-cols-3 mx-0 mt-6 gap-4" >
+                    <div >
+                        <Image
+                            src="/images/surabhi-Bg.png"
+                            width={400}
+                            height={300}
+                            alt="Picture of the author"
+                        />
+                        <h1 className="title text-2xl flex justify-center mt-5" >Dr. Surabjhi Vegad</h1>
+                        <h2 className="title flex justify-center mt-3" style={{color:'rgb(120,120,120)'}} >Laparoscopic Surgeon</h2>
+                        <h2 className="title text-center mt-5 ml-2 mr-2 leading-loose" style={{color:'rgb(120,120,120)'}} > Dr. Surabhi Vegad has been in practice for almost 20 years graduating BDS from the University of Manchester, UK in 1997</h2>
+                      
+                        <div className="flex justify-center ">
+                            <FontAwesomeIcon icon={faPhone} className="relative w-3 h-3 500 mt-6"style={{color:'rgb(51,104,198)'}} />
+                            <span className="ml-2 text-xl mt-3">+91-9989693645</span>
+                       
+                        </div>
+                        
+                    </div>
+                    <div>
+                        <Image
+                            src="/images/surabhi-Bg.png"
+                            width={400}
+                            height={300}
+                            alt="Picture of the author"
+                        />
+                         <h1 className="title text-2xl flex justify-center mt-5" >Dr. Surabjhi Vegad</h1>
+                        <h2 className="title flex justify-center mt-3" style={{color:'rgb(120,120,120)'}} >Laparoscopic Surgeon</h2>
+                        <h2 className="title text-center mt-5 ml-2 mr-2 leading-loose" style={{color:'rgb(120,120,120)'}} > Dr. Surabhi Vegad has been in practice for almost 20 years graduating BDS from the University of Manchester, UK in 1997</h2>
+                      
+                        <div className="flex justify-center ">
+                            <FontAwesomeIcon icon={faPhone} className="relative w-3 h-3 500 mt-6"style={{color:'rgb(51,104,198)'}} />
+                            <span className="ml-2 text-xl mt-3">+91-9989693645</span>
+                       
+                        </div>
+                    </div>
+                    <div className='mr-0'>
+                        <Image
+                            src="/images/surabhi-Bg.png"
+                            width={400}
+                            height={300}
+                            alt="Picture of the author"
+                        />
+                         <h1 className="title text-2xl flex justify-center mt-5" >Dr. Surabjhi Vegad</h1>
+                        <h2 className="title flex justify-center mt-3" style={{color:'rgb(120,120,120)'}} >Laparoscopic Surgeon</h2>
+                        <h2 className="title text-center mt-5 ml-2 mr-2 leading-loose" style={{color:'rgb(120,120,120)'}} > Dr. Surabhi Vegad has been in practice for almost 20 years graduating BDS from the University of Manchester, UK in 1997</h2>
+                      
+                        <div className="flex justify-center ">
+                            <FontAwesomeIcon icon={faPhone} className="relative w-3 h-3 500 mt-6"style={{color:'rgb(51,104,198)'}} />
+                            <span className="ml-2 text-xl mt-3">+91-9989693645</span>
+                       
+                        </div>
+                    </div></div>
+                </div>
+            </div> 
+        </div>
+    )
+}
