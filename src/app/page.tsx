0c57743e352bf8de46d56fbf789a8a9d5672098f
raymasahil <sahil@teque7.com>
2024-04-27 12:05:11 +0530

    homepage change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,6 @@ export default function Home() {
                       Read More <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-1">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                       </svg>
-
                   </button>
 
                   {/* <div className='flex items-center gap-4 mt-12'>
